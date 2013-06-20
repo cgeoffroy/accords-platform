b4fea246be0facd1bcf50792df269c888cb16262
cgeoffroy <cgeoffroy@tai.com>
2013-06-20 11:49:16 +0200

    Adding generic support for abstract objectstorage

---
+++ coobjectstorage/coobjectstorage.h
@@ -0,0 +1,10 @@
+#ifndef _coobjectstorage_h
+#define _coobjectstorage_h
+
+#include "rest.h"
+#include "accords.h"
+
+        /* --------- */
+#endif  /* _coobjectstorage_h */
+        /* --------- */
+
