c35ef5744dd315c3ac5a8d28ac90542a40a451b5
cgeoffroy <cgeoffroy@tai.com>
2013-06-20 11:49:16 +0200

    Adding generic support for abstract objectstorage

---
+++ coobjectstorage/coobjectstorage.c
@@ -0,0 +1,245 @@
+/* -------------------------------------------------------------------- */
+/*  ACCORDS PLATFORM                                                    */
+/*  (C) 2011 by Iain James Marshall (Prologue) <ijm667@hotmail.com>     */
+/* -------------------------------------------------------------------- */
+/* Licensed under the Apache License, Version 2.0 (the "License"); 	*/
+/* you may not use this file except in compliance with the License. 	*/
+/* You may obtain a copy of the License at 				*/
+/*  									*/
+/*  http://www.apache.org/licenses/LICENSE-2.0 				*/
+/*  									*/
+/* Unless required by applicable law or agreed to in writing, software 	*/
+/* distributed under the License is distributed on an "AS IS" BASIS, 	*/
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 	*/
+/* implied. 								*/
+/* See the License for the specific language governing permissions and 	*/
+/* limitations under the License. 					*/
+/* -------------------------------------------------------------------- */
+#ifndef	_coobjectstorage_c
+#define	_coobjectstorage_c
+
+#include "standard.h"
+#include "coobjectstorage.h"
+#include "rest.h"
+#include "occi.h"
+#include "document.h"
+#include "cordspublic.h"
+#include "occipublisher.h"
+#include "occibuilder.h"
+
+#include "bucket.h"
+#include "bucket.c"
+#include "occibucket.c"
+
+#include "objectstorage_manifest.h"
+
+struct	accords_configuration Coobjectstorage = {
+        0,1, /* monitor, auto publication */
+	1,1,0,0, /* verbose, debug, threads, ipv6 */
+	(char *) 0,
+	(char *) 0,
+	_CORDS_DEFAULT_PUBLISHER,
+	_CORDS_DEFAULT_OPERATOR,
+	_CORDS_DEFAULT_USER,
+	_CORDS_DEFAULT_PASSWORD,
+	"http",  80,
+	"xmpp",  8000,
+	"domain",
+	"coobjectstorage.xml",
+	"europe",
+	"storage",
+	"coobjectstorage",
+	(struct occi_category *) 0,
+	(struct occi_category *) 0
+	};
+
+public	int	check_debug()		{	return(Coobjectstorage.debug);		}
+public	int	check_verbose()		{	return(Coobjectstorage.verbose);		}
+public	char *	default_publisher()	{	return(Coobjectstorage.publisher);	}
+public	char *	default_operator()	{	return(Coobjectstorage.operator);	}
+public	char *	default_tls()		{	return(Coobjectstorage.tls);		}
+public	char *	default_zone()		{	return(Coobjectstorage.zone);		}
+
+public	int	failure( int e, char * m1, char * m2 )
+{
+	if ( e )
+	{
+		printf("\n*** failure %u",e);
+		if ( m1 )
+			printf(" : %s",m1);
+		if ( m2 )
+			printf(" : %s",m2);
+		printf(" **`\n");
+	}
+	return( e );
+}
+
+/*	---------------------------------------------	*/  
+/*			p r o c c i _ l o a d 		*/
+/*	---------------------------------------------	*/
+/*	this function loads coobjectstorage configuration	*/
+/*	from the xml configuration file.		*/
+/*	---------------------------------------------	*/  
+private	void	coobjectstorage_load()
+{
+	load_accords_configuration( &Coobjectstorage, "coobjectstorage" );
+	return;
+}
+
+private	int	banner()
+{
+	printf("\n   CompatibleOne Request Coobjectstorage : Version 1.0a.0.04");
+	printf("\n   Beta Version : 29/05/2013");
+	printf("\n   Copyright (c) 2013, 2012 Geoffroy, TAI");
+	printf("\n");
+	accords_configuration_options();
+	printf("\n\n");
+	return(0);
+
+}
+
+/*	------------------------------------------------------------------	*/
+/*			p r o c c i _ i n i t i a l i s e			*/
+/*	------------------------------------------------------------------	*/
+private	struct rest_server * coobjectstorage_initialise(  void * v,struct rest_server * sptr )
+{
+	struct	rest_extension * xptr;
+	if (!( xptr = rest_add_extension( sptr ) ))
+		return((struct rest_server *) 0);
+	else
+	{
+		xptr->net = (struct connection *) 0;
+		return( sptr );
+	}
+}
+
+/*	------------------------------------------------------------------	*/
+/*			p r o c c i _ a u t h o r i s e 			*/
+/*	------------------------------------------------------------------	*/
+private	int	coobjectstorage_authorise(  void * v,struct rest_client * cptr, char * username, char * password )
+{
+	if ( strcmp( username, Coobjectstorage.user ) )
+		return(0);
+	else if ( strcmp( password, Coobjectstorage.password ) )
+		return(0);
+	else if (!( cptr->user = allocate_string( username ) ))
+		return(0);
+	else if (!( cptr->pass = allocate_string( password ) ))
+		return(0);
+	else	return(1);
+}
+
+/*	------------------------------------------------------------------	*/
+/*			p r o c c i _ e x t e n s i o n 			*/
+/*	------------------------------------------------------------------	*/
+private	struct rest_extension * coobjectstorage_extension( void * v,struct rest_server * sptr, struct rest_extension * xptr)
+{
+	return( xptr );
+}
+
+
+/*	------------------------------------------------------------------	*/
+/*			p r o c c i _ o p e r a t i o n				*/
+/*	------------------------------------------------------------------	*/
+private	int	coobjectstorage_operation( char * nptr )
+{
+
+	struct	occi_category * first=(struct occi_category *) 0;
+	struct	occi_category * last=(struct occi_category *) 0;
+	struct	occi_category * optr=(struct occi_category *) 0;
+
+	set_autosave_cords_xlink_name("links_swift.xml");
+
+	rest_initialise_log( Coobjectstorage.monitor );
+
+	if (!( optr = occi_bucket_builder( Coobjectstorage.domain, "objectstorage_manifest" ) ))
+                return( 27 );
+        else if (!( optr->previous = last ))
+                first = optr;
+        else    optr->previous->next = optr;
+        last = optr;
+
+        if (!( optr = occi_bucket_builder( Coobjectstorage.domain, "bucket" ) ))
+                return( 27 );
+        else if (!( optr->previous = last ))
+                first = optr;
+        else    optr->previous->next = optr;
+        last = optr;
+
+	if (!( Coobjectstorage.identity )) {
+		/* --------------------------   */
+                /* launch simple occi server    */
+                /* --------------------------   */
+		return( occi_server(  nptr, Coobjectstorage.restport, 
+				      Coobjectstorage.tls, Coobjectstorage.threads,
+				      first, (char *) 0 ) );
+	} else {
+		/* --------------------------------------------------------------- */
+                /* publication and pricing of categories before occi server launch */
+                /* --------------------------------------------------------------- */
+
+		initialise_occi_publisher( Coobjectstorage.publisher, (char*) 0, (char *) 0, (char *) 0);
+		return( publishing_occi_server(
+			Coobjectstorage.user, Coobjectstorage.password,
+			Coobjectstorage.identity,  nptr, 
+			Coobjectstorage.restport, Coobjectstorage.tls, 
+			Coobjectstorage.threads, first ) );
+	}
+}
+
+
+/*	------------------------------------------------------------------	*/
+/*				p r o c c i 					*/
+/*	------------------------------------------------------------------	*/
+private	int	coobjectstorage(int argc, char * argv[] )
+{
+	int	status=0;
+	int	argi=0;
+	char *	aptr;
+	coobjectstorage_load();
+	while ( argi < argc )
+	{
+		if (!( aptr = argv[++argi] ))
+			break;
+		else if ( *aptr == '-' )
+		{
+			aptr++;
+			switch( *(aptr++) )
+			{
+			case	'v'	:
+				Coobjectstorage.verbose=1;
+				continue;
+			case	'd'	:
+				Coobjectstorage.debug = 0xFFFF;
+				continue;
+			case	'-'	:
+				if (!( argi = accords_configuration_option( aptr, argi, argv )))
+					break;
+				else	continue;
+			}
+			status = 30;
+			break;
+		}
+		else if (!( status = coobjectstorage_operation(aptr) ))
+			continue;
+		else	break;
+	}
+	return(status);
+}
+
+/*	------------------------------------------------------------------	*/
+/*					m a i n 				*/
+/*	------------------------------------------------------------------	*/
+public	int	main(int argc, char * argv[] )
+{
+	if ( argc == 1 )
+		return( banner() );
+	else	return( coobjectstorage( argc, argv ) );
+
+}
+
+
+	/* --------- */
+#endif	/* _coobjectstorage_c */
+	/* --------- */
+
